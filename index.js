10965bebc3bfbfb4ef1ee879480fad8741f16b93
Viktor Stoimenov <viktorstoimenov@vivaldi.net>
2022-03-16 19:42:58 +0200

    Biggus Particulus Poggus

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 console.log("Js Initialised");
 let gFrame = document.getElementsByClassName("gFrame");
 
-const blob = [
-  getComputedStyle(document.documentElement).getPropertyValue("--blob1"),
-  getComputedStyle(document.documentElement).getPropertyValue("--blob2"),
-  getComputedStyle(document.documentElement).getPropertyValue("--blob3"),
-  getComputedStyle(document.documentElement).getPropertyValue("--blob4"),
-  getComputedStyle(document.documentElement).getPropertyValue("--blob5"),
-];
-
 for (let i = 0; i < gFrame.length; i++) {
   //@ts-ignore
   gFrame[i].style.borderRadius = randomRadius(45);
@@ -38,3 +30,7 @@ function getRandomInt(max) {
   //   return Math.floor(Math.random() * max);
   return Math.random() * (max - 15) + 15; //random number more than 15 but no more than max
 }
+//@ts-ignore
+particlesJS.load("stars", "/particlesjs-config.json", function () {
+  console.log("callback - particles.js config loaded");
+});
