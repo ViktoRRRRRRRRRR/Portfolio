ed3328b38a24bb05def9644cd1e293c5fa9ccd69
Viktor Stoimenov <viktorstoimenov@vivaldi.net>
2022-03-21 22:19:15 +0200

    About me section finished?

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-//@ts-check
-console.log("Js Initialised");
-let gFrame = document.getElementsByClassName("gFrame");
+'use strict';
+console.log('Js Initialised');
+const gFrame = document.getElementsByClassName('gFrame');
 
-for (let i = 0; i < gFrame.length; i++) {
-  //@ts-ignore
-  gFrame[i].style.borderRadius = randomRadius(45);
-}
-
-/**
- *
- * @param {number} num defines the max percantage of curvyness
- */
-function randomRadius(num) {
-  let rad = "";
-  for (let i = 0; i < 8; i++) {
-    rad += getRandomInt(num) + "% ";
-    if (i == 3) {
-      rad += "/ ";
-    }
-  }
-  return rad;
-}
 /**
  *
  * @param {number} max upper random ceiling
@@ -30,7 +11,26 @@ function getRandomInt(max) {
   //   return Math.floor(Math.random() * max);
   return Math.random() * (max - 15) + 15; //random number more than 15 but no more than max
 }
-//@ts-ignore
-particlesJS.load("stars", "/Particles/particlesjs-config.json", function () {
-  console.log("callback - particles.js config loaded");
+
+/**
+ *
+ * @param {number} num defines the max percantage of curvyness
+ */
+function randomRadius(num) {
+  let rad = '';
+  for (let i = 0; i < 8; i++) {
+    rad += getRandomInt(num) + '% ';
+    if (i === 3) {
+      rad += '/ ';
+    }
+  }
+  return rad;
+}
+
+for (let i = 0; i < gFrame.length; i++) {
+  gFrame[i].style.borderRadius = randomRadius(45);
+}
+
+particlesJS.load('stars', '/Particles/particlesjs-config.json', function () {
+  console.log('callback - particles.js config loaded');
 });
